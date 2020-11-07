65c90f39ce077437238353b11b280afffe47ceaa
Nourhan Saad <nourhansaad192@gmail.com>
2020-11-07 11:49:32 +0200

    first

---
+++ reen/js/reen.js
@@ -0,0 +1,23 @@
+// JavaScript source code
+$(document).ready(function(){
+  $('.carousel').slick({
+  slidesToShow: 1,
+  dots:true,
+  fade:true,
+  
+  });
+
+    $('.carousel_2').slick({
+  slidesToShow: 1,
+  dots:true,
+ 
+  
+  });
+
+
+
+
+
+
+
+});
